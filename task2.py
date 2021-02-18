72d1295e8597b620da7b7386ca6799af3990f2cc
Ziii-l <ziweil101@deltalearns.ca>
2021-02-17 23:38:38 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -15,13 +15,12 @@ outputs:
 Access granted
 Access denied
 """
-username = "admin"
-password =12345
+username = ""
+password =""
 
-while username == "admin" and password == 12345:
+while username != "admin" and password != "12345":
     username=str(input("please enter username"))
-    if username== "admin":
-        print("Access granted")
     password=str(input("please enter password"))
-    if password == "12345":
+    if username != "admin" and password != "12345":
         print("Access denied")
+print("Access granted")   
